a295a648c39d076f1398f64c46cc34544e068728
Miguel Angel <miguel9196.guzman@gmail.com>
2023-06-04 21:24:56 -0500

    creacion de funciones para conexion,T Riemman, T Ricci y escalr de curvatura

--- metric_01.py
+++ metric_01.py
@@ -21,20 +21,109 @@ os.system("clear")
 
 #------------------Variables_simbolicas----------------------#
 
-var     =    ["r", "phi", "theta"]
+var     =    ["psi", "theta", "phi"]
+r       =    symbols("r")
 var     =    symbols(var)
 n       =    len(var)
 
 #---------------Tensor_Metrico_y_inverso---------------------#
 
-g       =    [[1,0,0],[0,(var[0]**2)*(sin(var[2])**2),0],[0,0,var[0]**2]]
+g       =    [[(r**2),0,0],[0,(r**2)*(sin(var[0])**2),0],[0,0,(r**2)*(sin(var[0])**2)*(sin(var[1])**2)]]
 G       =    Array(g)
-Ginv    =    simplify(Array(Matrix(g).inv()))
+Ginv    =    simplify(factor(Array(Matrix(g).inv())))
 
 #-----------Simbolos_de_Christofell_Clase_1------------------#
 
 def christofell(i,j,k):
-    ch  =   simplify( (1/2)*( diff(G[j,k],var[i]) + diff(G[i,k],var[j]) - diff(G[i,j],var[k]) ) )
+    ch  =   simplify(factor( ( diff(G[j,k],var[i]) + diff(G[i,k],var[j]) - diff(G[i,j],var[k]) )/2 ))
     return ch
 
-print(christofell(0,1,1))
+#-----------Simbolos_de_Christofell_Clase_2------------------#
+
+def conexion(k,j,i):
+    cx   =   0
+    for s in range(n):
+        D    =   Ginv[k,s]*christofell(i,j,s) 
+        cx   =   simplify(factor(cx + D)) 
+    return cx
+
+#-----------Tensor_de_Riemann_4-Covariante------------------#
+
+def riemann(p,i,j,k):
+
+    DD = [0]*n 
+    for u in range(n):
+        DD[u]   =   [0]*n    
+
+    DDD = [0]*n 
+    for u in range(n):
+        DDD[u]   =   [0]*n 
+
+    for u1 in range(n):
+        for u2 in range(n):
+            DD[u1][u2] = DDD
+
+    for s in range(n):
+        DD[p][i][j][k]  =   factor(DD[p][i][j][k] + conexion(s,k,p)*christofell(i,j,s) - conexion(s,j,p)*christofell(i,k,s))
+
+    riem    =   factor(diff(christofell(i,k,p), var[j]) - diff(christofell(i,j,p), var[k]) + DD[p][i][j][k])
+    riem    =   simplify(riem)
+    return riem
+
+#------Tensor_de_Riemann_3-Covariante_1-Contravariante------#
+
+def TRiemann(p,i,j,k):
+    TR      =   0
+    for s in range(n):
+        TR  =   TR + Ginv[p,s]*riemann(s,i,j,k)
+        TR  =   simplify(factor(TR))
+    return TR
+
+#--------------------Tensor_de_Ricci------------------------#
+
+def ricci(i,j):
+    ric    =   0
+    for s in range(n):
+        ric    =   ric + TRiemann(s,i,s,j)
+        ric    =   simplify(factor(ric))
+    return ric
+
+#------------------Escalar_de_Curvatura---------------------#
+
+def Rcurva():
+    curv    =   0
+    for i in range(n):
+        for j in range(n):
+            curv    =   curv + Ginv[i,j]*ricci(i,j)
+            curv    =   simplify(factor(curv))
+    return curv
+
+#-----------------------------------------------------------#
+
+"""
+print("Tensor metrico")
+print(G)
+
+print("Tensor metrico inverso")
+print(Ginv)
+
+print("Simblos de Christofell de primera clase")
+print("0,1,1: ", christofell(0,1,1))
+
+print("Simblos de Christofell de segunda clase")
+print("0,1,1: ", conexion(0,1,1))
+
+print("Tensor de Riemann 4-covarinte")
+print("0,2,0,2: ", riemann(0,2,0,2))
+
+print("Tensor de Riemann 3-covarinte y 1-contravariante")
+print("0,2,0,2: ", TRiemann(0,2,0,2))
+
+print("Tensor de Ricci")
+print("2,2: ", ricci(2,2))
+
+print("Escalar de Curvatura")
+print(Rcurva())
+"""
+
+
