4592caa7f13befed712b45c479aab7a66c004730
Miguel Angel <miguel9196.guzman@gmail.com>
2023-06-09 14:54:19 -0500

    Retoques pdf

--- metric3.py
+++ metric3.py
@@ -25,26 +25,26 @@ from metric2 import*
 
 #----------------Definicion_por_componentes------------------#
 
-def metric(i,j):
-    return G[i,j] 
+# def metric(i,j):
+#     return G[i,j] 
 
-def metric_inv(i,j):
-    return Ginv[i,j]
+# def metric_inv(i,j):
+#     return Ginv[i,j]
 
-def Christofell(k,j,i):
-    return VChristofell[k,j,i]
+# def Christofell(k,j,i):
+#     return VChristofell[k,j,i]
 
-def Conexion(k,j,i):
-    return VConexion[k,j,i]
+# def Conexion(k,j,i):
+#     return VConexion[k,j,i]
 
-def riemann(p,k,j,i): 
-    return Vriemann[p,k,j,i]
+# def riemann(p,k,j,i): 
+#     return Vriemann[p,k,j,i]
 
-def Riemann(p,k,j,i):
-    return VRiemann[p,k,j,i]
+# def Riemann(p,k,j,i):
+#     return VRiemann[p,k,j,i]
 
-def Ricci(i,j):
-    return VRicci[i,j]
+# def Ricci(i,j):
+#     return VRicci[i,j]
 
 def EscalarC():
     return VEscalarC
@@ -77,7 +77,19 @@ def Ricci_Mt():
 ##--------------Construcion_del_document_pdf----------------##
 ##############################################################
 
-doc     =   Document()
+doc     =   Document()                                    
+doc.preamble.append(NoEscape(r"\usepackage[utf8]{inputenc}"))
+doc.preamble.append(NoEscape(r"\usepackage[spanish]{babel}"))
+doc.preamble.append(NoEscape(r"\usepackage{amsmath}"))
+doc.preamble.append(NoEscape(r"\usepackage{amsfonts}"))
+doc.preamble.append(NoEscape(r"\usepackage{amssymb}"))
+doc.preamble.append(NoEscape(r"\usepackage{graphicx}"))
+doc.preamble.append(NoEscape(r"\usepackage{txfonts}"))
+doc.preamble.append(NoEscape(r"\usepackage{mathrsfs}"))
+doc.preamble.append(NoEscape(r"\usepackage{color}"))
+doc.preamble.append(NoEscape(r"\usepackage{parskip}"))
+doc.preamble.append(NoEscape(r"\usepackage[spanish]{babel}"))
+doc.preamble.append(NoEscape(r"\usepackage[left=2cm,right=2cm,top=2cm,bottom=2cm]{geometry}"))
 
 #------------Reescritura_de_funciones_a_latex----------------#
 
@@ -116,36 +128,45 @@ print("Construcion del documento pdf")
 
 with doc.create(Section("Tensor Metrico y inverso")):
     doc.append(Math(data=[NoEscape("g_{ij} = " + latex_metric)]))
+    doc.append("\n")
     doc.append(Math(data=[NoEscape("g^{ij} = " + latex_metric_int)]))
+    doc.append("\n")
 
 with doc.create(Section("Simbolos de Christofell Clase 1")):
     for i in range(n):
         doc.append(Math(data=[NoEscape(latex_chritofell[i])]))
-
+        doc.append("\n")
+ 
 with doc.create(Section("Simbolos de Christofell Clase 2")):
     for i in range(n):
         doc.append(Math(data=[NoEscape(latex_conexion[i])]))
-
+        doc.append("\n")
+ 
 with doc.create(Section("Tensor de Riemann 4-Covariante")):
     for i in range(n*n):
         doc.append(Math(data=[NoEscape(latex_rimann[i])]))
-
+        doc.append("\n")
+ 
 with doc.create(Section("Tensor de Riemann 3-Covariante 1-Contravariante")):
     for i in range(n*n):
         doc.append(Math(data=[NoEscape(latex_Rimann[i])]))
+        doc.append("\n")
 
 with doc.create(Section("Tensor de Ricci")):
     doc.append(Math(data=[NoEscape(latex_Ricci)]))
+    doc.append("\n")
 
 with doc.create(Section("Escalar de Curvatura")):
     doc.append(Math(data=[NoEscape(Latex_Escalar)]))
+    doc.append("\n")
 
-print("Complete \n")
 
 #--------------------Generando_pdf---------------------------#
 
 doc.generate_pdf("Metric_doc", clean_tex=True)                  #True: borra todos los archivos de latex una vez cerado el pdf
 os.system("zathura Metric_doc.pdf &")
+
+print("Complete \n")
 
 #------------------------------------------------------------#
 
