18c535a1b20406c1433d41cbfb393a16da4f2dea
Miguel Angel <miguel9196.guzman@gmail.com>
2023-06-11 23:57:38 -0500

    Parametros de entrada

--- metric1.py
+++ metric1.py
@@ -41,18 +41,64 @@ os.system("clear")
 ##-----------------Parametros_de_entrada--------------------##
 ##############################################################
 
-#------------------Variables_simbolicas----------------------#
 
-var     =    ["psi", "theta", "phi"]                                                                            #insert variables
-r       =    symbols("r")
+##############################################################
+##-----------------Variables_simbolicas---------------------##
+##############################################################
+
+#---------------------Insert_variables-----------------------#
+
+n       =   int(input("Ingrese el numero de dimenciones: "))
+var     =    []                                             
+for i in range(n):
+        var.append(str(input(f"Variable {i+1}: ")))
 var     =    symbols(var)
-n       =    len(var)
+print()
 
-#---------------Tensor_Metrico_y_inverso---------------------#
+#-------------------variables_adicionales--------------------#
+ss      = "¿cuantos valores constantes posee el tensor metrico? : "
+NN       = int(input(ss))
+varr     =   []      
+for i in range(NN):
+    varr.append(str(input(f"Constante {i+1}: ")))
+varr    =    symbols(varr)
+print()
 
-g       =    [[(r**2),0,0],[0,(r**2)*(sin(var[0])**2),0],[0,0,(r**2)*(sin(var[0])**2)*(sin(var[1])**2)]]        #inset T metric
+
+##############################################################
+##--------------Tensor_Metrico_y_inverso--------------------##
+##############################################################
+
+#--------------------Tensor_metrico--------------------------#
+
+A   =  []
+B   =  []
+
+print("introduce el tensor metrico:")
+for i in range(n):
+    for j in range(n):
+        if i <= j: 
+            A.append(input(f"Introduc G{i+1}{j+1}: "))
+        elif i > j: 
+            A.append(0)
+A   =   Array(A, (n,n))
+
+for i in range(n):
+    for j in range(n):
+        if i > j:
+            B.append(A[j,i])
+        elif i <= j:
+            B.append(0)
+B   =   Array(B, (n,n))
+
+g   =   A + B
 G       =    Array(g)
+
+#--------------Tensor_metrico _inverso-----------------------#
+
 Ginv    =    simplify(factor(Array(Matrix(g).inv())))
+
+os.system("clear")
 
 #------------------------------------------------------------#
 
---
+++ readme.txt
@@ -0,0 +1,20 @@
+librerias python:
+sympy
+pylatex
+
+programas arch a instalar:
+sudo pacman -S zathura
+sudo pacman -S texlive-most
+
+compilar latex : 
+pdflatex nombre_del_archivo.tex
+
+compilador python:
+pip install pyinstaller 
+
+compilar python:
+pyinstaller nombre_archivo.py
+
+buscar ruta de un programa:
+which nombre_programa
+
