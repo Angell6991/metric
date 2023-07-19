53b3e52098c132a0c4cd40a29398f400bb9ab891
Miguel Angel <miguel9196.guzman@gmail.com>
2023-07-18 19:07:22 -0500

    objeto

--- metric1.py
+++ metric1.py
@@ -17,26 +17,11 @@
  ############################################################
 
 from PIL.Image import Image
-import sympy as sy
 import PIL as P
 import customtkinter as ctk
 import os
 
 os.system("clear")
-
-##############################################################
-#--------------------Guia_de_funciones-----------------------#
-#                                                            #
-#   Tensor metrico                      metric_Mt()          #
-#   T metrico inverso                   metric_inv_Mt()      #
-#   Simb Christofell clase 1            Christofell_Mt(k)    #
-#   Simb Christofell Clase 2            Conexion_Mt(k)       #
-#   T Riemann 4-Cova                    riemann_Mt(i,j)      #
-#   T Riemann 3-Cova y 1-Contrava       Riemann_Mt(i,j)      #
-#   T Ricci                             Ricci_Mt()           #
-#   Escalar de Curvatura                EscalarC()           #
-#                                                            #
-##############################################################
 
 ##############################################################
 ##-------------------Inicio_modo_Visual---------------------##
@@ -118,68 +103,4 @@ boton       =   ctk.CTkButton(
 boton.place(relx=0.5, rely=0.62, anchor=ctk.CENTER)
 
 ventana.mainloop()
-
-
-##############################################################
-##-----------------Variables_simbolicas---------------------##
-##############################################################
-
-#---------------------Insert_variables-----------------------#
-
-n       =   int(u)
-var     =    []                                             
-for i in range(n):
-        var.append(str(input(f"Variable {i+1}: ")))
-var     =    sy.symbols(var)
-print(var)
-
-#-------------------variables_adicionales--------------------#
-"""
-NN       = int(input(uu))
-varr     =   []      
-for i in range(NN):
-    varr.append(str(input(f"Constante {i+1}: ")))
-varr    =    symbols(varr)
-print()
-
-
-
-##############################################################
-##--------------Tensor_Metrico_y_inverso--------------------##
-##############################################################
-
-#--------------------Tensor_metrico--------------------------#
-
-A   =  []
-B   =  []
-
-print("introduce el tensor metrico:")
-for i in range(n):
-    for j in range(n):
-        if i <= j: 
-            A.append(input(f"Introduc G{i+1}{j+1}: "))
-        elif i > j: 
-            A.append(0)
-A   =   Array(A, (n,n))
-
-for i in range(n):
-    for j in range(n):
-        if i > j:
-            B.append(A[j,i])
-        elif i <= j:
-            B.append(0)
-B   =   Array(B, (n,n))
-
-g   =   A + B
-G       =    Array(g)
-
-#--------------Tensor_metrico _inverso-----------------------#
-
-Ginv    =    simplify(factor(Array(Matrix(g).inv())))
-
-os.system("clear")
-
-#------------------------------------------------------------#
-
-"""
 
--- metric2.py
+++ metric2.py
@@ -13,68 +13,165 @@
 ###                                                        ###
 ##############################################################
  ############################################################
- ##------------------metric_part_2-------------------------##
- ############################################################
 
-from metric1 import*
+import sympy as sy
+import pandas as pd
+import os
 
 ##############################################################
-##----------Simbolos_de_Christofell_Clase_1-----------------##
+##-------------------Guia_de_funciones----------------------##
+##############################################################
+##                                    |                     ##
+##   Tensor metrico                   |   metric_Mt()       ##
+##   T metrico inverso                |   metric_inv_Mt()   ##
+##   Simb Christofell clase 1         |   Christofell_Mt(k) ##
+##   Simb Christofell Clase 2         |   Conexion_Mt(k)    ##
+##   T Riemann 4-Cova                 |   riemann_Mt(i,j)   ##
+##   T Riemann 3-Cova y 1-Contrava    |   Riemann_Mt(i,j)   ##
+##   T Ricci                          |   Ricci_Mt()        ##
+##   Escalar de Curvatura             |   EscalarC()        ##
+##                                    |                     ##
+##############################################################
 ##############################################################
 
-def christofell(k,j,i):
-    ch  =   (
-             diff(G[j,k],var[i]) + 
-             diff(G[i,k],var[j]) - 
-             diff(G[i,j],var[k]) 
-             )/2 
+class metric:
+
+    def __init__(self, variables, no_variables, Tensor_metric):
+        
+        #------------------------Insert_data-------------------------#
+        
+        #---------------------Insert_variables-----------------------#
+        self.lista1      =      pd.read_csv(variables, header = None)
+        self.lista1      =      self.lista1.values.tolist()
+        self.n           =      len(self.lista1)
+        
+        self.var         =      []
+        for i in range(self.n):
+            self.var.append(sy.symbols(self.lista1[i][0]))
+        
+        self.var         =      sy.Array(self.var)
+            
+
+        #----------------------Insert_constants----------------------#
+        self.lista2     =       pd.read_csv(no_variables, header = None)
+        self.lista2     =       self.lista2.values.tolist()
+        
+        self.varr       =       []
+        for i in range(len(self.lista2)):
+            self.varr.append(sy.symbols(self.lista2[i][0]))
+        
+        self.varr       =      sy.Array(self.varr)
+       
+
+        #--------------------Insert_tensor_metric--------------------#
+        self.lista3     =       pd.read_csv(Tensor_metric, header = None)
+        self.lista3     =       self.lista3.values.tolist()
+
+        
+    ##############################################################
+    ##--------------Tensor_Metrico_y_inverso--------------------##
+    ##############################################################
+
+    #--------------------Tensor_metrico--------------------------#
+
+    def metric_Mt(self):
+        
+        A   =  []
+        for i in range(self.n):
+            for j in range(self.n):
+                if i <= j:
+                    A.append(self.lista3[i+j][0])
+                elif i > j:
+                    A.append(0)
+
+        A   =   sy.Array(A,(self.n,self.n))
+
+        B   =  []
+        for i in range(self.n):
+            for j in range(self.n):
+                if i > j:
+                    B.append(A[j,i])
+                elif i <= j:
+                    B.append(0)
+        B   =   sy.Array(B, (self.n,self.n))
+
+        g   =   A + B
+        G       =    sy.Array(g)
+
+        return G
     
-    #ch  =   simplify(factor(ch))
-    return ch
-
-#------------------------Visual------------------------------#
-
-print("Simbolos de Christofell Clase 1")
-
-VChristofell  =   []
-
-for k in range(n):
-    for j in range(n):
-        for i in range(n):
-            VChristofell.append(christofell(k,j,i)) 
-
-VChristofell  =   Array(VChristofell, (n,n,n))
-
-print("Complete \n")
+    #--------------Tensor_metrico _inverso-----------------------#
+    def metric_inv_Mt(self):
+        Gin  =   sy.simplify(sy.factor(sy.Matrix(self.metric_Mt()).inv()))
+        return sy.Array(Gin)
 
 
-##############################################################
-##-----------Simbolos_de_Christofell_Clase_2----------------##
-##############################################################
+    ##############################################################
+    ##----------Simbolos_de_Christofell_Clase_1-----------------##
+    ##############################################################
 
-def conexion(i,j,k):
-    cx   =   0
-    for s in range(n):
-        D    =   Ginv[k,s]*VChristofell[s,j,i] 
-        cx   =   cx + D
-        #cx   =   simplify(factor(cx + D)) 
-    return cx
+    def Christofell_Mt(self,s):
+        G   =   self.metric_Mt()
+        var =   self.var
 
-#------------------------Visual------------------------------#
+        def christofell(k,j,i):
+            ch  =   (
+                    sy.diff(G[j,k],var[i]) + 
+                    sy.diff(G[i,k],var[j]) - 
+                    sy.diff(G[i,j],var[k]) 
+                    )/2 
+            return ch
 
-print("Simbolos de Christofell Clase 2")
+        VChristofell  =   []
 
-VConexion  =   []
+        for k in range(self.n):
+            for j in range(self.n):
+                for i in range(self.n):
+                    VChristofell.append(christofell(k,j,i)) 
 
-for k in range(n):
-    for j in range(n):
-        for i in range(n):
-            VConexion.append(conexion(i,j,k)) 
+        VChristofell  =   sy.Array(VChristofell, (self.n, self.n ,self.n))
 
-VConexion  =   Array(VConexion, (n,n,n))
+        return VChristofell[s]
 
-print("Complete \n")
 
+    ##############################################################
+    ##-----------Simbolos_de_Christofell_Clase_2----------------##
+    ##############################################################
+
+    def Conexion_Mt(self,s):
+        
+        Ginv            =      self.metric_inv_Mt()
+
+        def conexion(i,j,k):
+            cx   =   0
+            for p in range(self.n):
+                D    =   Ginv[k,p]*self.Christofell_Mt(p)[j,i] 
+                cx   =   cx + D
+                #cx   =   simplify(factor(cx + D)) 
+            return cx
+
+        VConexion  =   []
+
+        for k in range(self.n):
+            for j in range(self.n):
+                for i in range(self.n):
+                    VConexion.append(conexion(i,j,k)) 
+
+        VConexion  =   sy.Array(VConexion, (self.n, self.n, self.n))
+
+
+        return VConexion[s]
+
+
+#---istanciando_objeto---#
+
+os.system("clear")
+
+UU  =   metric("variables.dat","no_variables.dat","tensor_metrico.dat")
+print(UU.Conexion_Mt(0))
+print(UU.Conexion_Mt(1))
+
+"""
 
 #############################################################
 ##----------Tensor_de_Riemann_4-Covariante-----------------##
@@ -182,17 +279,7 @@ print("Complete \n")
 ##--------Definicion_de_funciones_en_forma_matricial--------##
 ##############################################################
 
-def metric_Mt():
-    return G 
 
-def metric_inv_Mt():
-    return Ginv
-
-def Christofell_Mt(k):
-    return VChristofell[k]
-
-def Conexion_Mt(k):
-    return VConexion[k]
 
 def riemann_Mt(j,i): 
     return Vriemann[j,i]
@@ -208,5 +295,5 @@ def EscalarC():
 
 #------------------------------------------------------------#
 
-
+"""
 
